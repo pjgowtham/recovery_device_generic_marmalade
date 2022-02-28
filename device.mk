65d5d827deea063fb184d10bb78bcfd7d8c2cbb4
Michael Bestas <mkbestas@lineageos.org>
2022-02-28 22:51:41 +0200

    marmalade: bootctrl: Remove bootctrl.$(PRODUCT_PLATFORM) target
    * This is no longer used after switching to QTI 1.1 impl
    
    Change-Id: Id770b77da62c3b16eaea83cec366b299b1ea49d2

--- device.mk
+++ device.mk
@@ -76,8 +76,7 @@ TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    bootctrl.$(PRODUCT_PLATFORM).recovery \
+    android.hardware.boot@1.1-impl-qti.recovery
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
