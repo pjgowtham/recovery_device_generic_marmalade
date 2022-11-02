6563cc9986b06981c040a026c45d2d08218371c1
pjgowtham <pjgowtham@gmail.com>
2022-11-02 21:32:24 +0530

    marmalade: Load props from odm && hardcode known devices
    * Oplus devices now load properties from odm/build.prop since android-13 instead of odm/etc/$(getprop ro.boot.prjname)
    * Custom asserts are hardcoded for known devices to mitigate flash fails

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,6 +160,7 @@ TW_NO_HAPTICS := true
 TW_NO_SCREEN_BLANK := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_RESETPROP := true
+TW_OVERRIDE_PROPS_ADDITIONAL_PARTITIONS := odm
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 RECOVERY_LIBRARY_SOURCE_FILES += \
--- recovery/root/system/bin/load_oplus_props.sh
+++ recovery/root/system/bin/load_oplus_props.sh
@@ -3,6 +3,7 @@
 
 oplus_project=$(getprop ro.boot.prjname)
 DIR="/odm/etc/$oplus_project"
+generic_device_prop=$(getprop ro.product.device)
 
 if [ -d "$DIR" ]; then
   echo "load_oplus_props: ODM already mounted, proceeding to load" > /tmp/recovery.log
@@ -12,7 +13,40 @@ else
   mount /odm
 fi
 
+resetprop --file /odm/etc/build.prop
+resetprop --file /odm/build.prop
 resetprop --file $DIR/build.default.prop
 umount /odm
+
+# Custom device asserts
+resetprop ro.twrp.target.devices $(getprop ro.product.odm.device),$(getprop ro.vendor.product.device.oem),$(getprop ro.vendor.product.oem)
+
+# Fallback to hardcoded asserts to support known devices to mitigate cases where ROM flashing goes awry
+case $oplus_project in
+     "20664"|"20627") # Realme GT
+          resetprop ro.twrp.target.devices RMX2202,RMX2202L1,RMX2202CN
+          resetprop ro.product.device RMX2202
+          ;;
+     "20821") # Oneplus 9RT
+          resetprop ro.twrp.target.devices OP5155,OP5155L1,MT2111
+          resetprop ro.product.device OP5155
+          ;;
+     "21075") # Oppo K9S
+          resetprop ro.twrp.target.devices OP524F,PERM10
+          resetprop ro.product.device OP524F
+          ;;
+     "21643"|"21644"|"21695") # Realme Q3S
+          resetprop ro.twrp.target.devices RMX3461,RE548B,RMX3461T2,RE548BL1,RE54BFL1
+          resetprop ro.product.device RMX3461
+          ;;
+     "21675") # Realme GT Master Edition
+          resetprop ro.twrp.target.devices RMX3360,RMX3360L1,RE54ABL1,RMX3363
+          resetprop ro.product.device RMX3360
+          ;;
+     "216B2") # Realme GT2
+          resetprop ro.twrp.target.devices RMX3312,,RE58B2L1
+          resetprop ro.product.device RMX3312
+          ;;
+esac
 
 exit 0
