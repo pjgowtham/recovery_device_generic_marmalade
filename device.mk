f8ead02a103154d198b70c2434dc32d3406bcff0
ZVNexus <akoskovich@pm.me>
2022-11-19 10:13:16 +0530

    marmalade: Update to Boot Control HAL v1.2

--- device.mk
+++ device.mk
@@ -76,7 +76,7 @@ TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti.recovery
+    android.hardware.boot@1.2-impl-qti.recovery
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
